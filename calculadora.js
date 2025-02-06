1c8002da3e99a5df7f45a7d8a30933f1807c860d
AriRzbrf <ariane.chaves.ribeiro@escola.pr.gov.br>
2025-02-06 09:29:52 -0300

    Add files via upload

---
+++ calculadora.js
@@ -0,0 +1,16 @@
+let num1 = parseFloat (prompt ("digite o primeiro numero"));
+let num2 = parseFloat (prompt ("digite o segundo numero"));
+let  soma = num1 + num2;
+let subtracao = num1 - num2;
+let multiplicacao = num1 * num2;
+let divisao = num1 / num2;
+
+ console.log ('soma: ${soma}');
+ console.log ('subtração: ${subtracao}');
+ console.log ('multiplicação: $ {multiplicacao}');
+ console.log ('divisão:${divisao}');
+ 
+ alert ('subtração : ${soma}');
+ alert ('subtração: ${subtracao}');
+alert ('multiplicação: ${multiplicacao}');
+alert ('divisão: ${divisao}');
